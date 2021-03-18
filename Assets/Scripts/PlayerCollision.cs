0baffca3cec3b9296acd2e054bfe87df35099fae
alexandrejanin <alexandrejanin92@gmail.com>
2021-03-18 16:49:16 +0100

    tu coné

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
+    [SerializeField] private LayerMask layerMask;
     [SerializeField] private GameObject defeatMenu;
 
     private void OnControllerColliderHit(ControllerColliderHit hit) {
-        if (hit.gameObject.CompareTag("Defeat")) {
+        if ((layerMask & 1 << hit.gameObject.layer) != 0) {
             GetComponent<PlayerMovement>().enabled = false;
             defeatMenu.SetActive(true);
         }
--- Assets/Scripts/PlayerDodge.cs
+++ Assets/Scripts/PlayerDodge.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 
 public class PlayerDodge : MonoBehaviour {
+    [SerializeField] private LayerMask layer;
+    [SerializeField] private float sideLength = 0.7f;
+
+    private void Update() {
+        if (
+            Physics.Raycast(transform.position, -transform.right, sideLength, layer)
+            || Physics.Raycast(transform.position, transform.right, sideLength, layer)
+        ) {
+            Debug.Log("Dodge!");
+        }
+    }
+
     private void OnDrawGizmos() {
-        Gizmos.DrawRay(transform.position - 0.5f * transform.right, -transform.right);
-        Gizmos.DrawRay(transform.position + 0.5f * transform.right, transform.right);
+        Gizmos.DrawRay(transform.position, -sideLength * transform.right);
+        Gizmos.DrawRay(transform.position, sideLength * transform.right);
     }
 }
